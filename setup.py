d3b7d0a909826a3742505853af7cb58cbf02e6a0
Darwin Smith II <dwin@dlsmi.com>
2019-12-08 18:23:42 -0500

    Update Version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="xid-py",
-    version="1.1.0",
+    version="1.1.1",
     author="Darwin Smith II",
     author_email="pythonxid@dwin.fastmail.com",
     description="Python implementation of XID globally unique id generator",
